b9539e6bf51f720957b8dac7be71a386f406807d
20175223Yogile <575969143@qq.com>
2019-03-05 19:42:49 +0800

    sent new_week2_work_19.3.5

---
+++ work/exp4_1.java
@@ -0,0 +1,18 @@
+class xyr
+{
+	float height,weight;
+	String head, ear;
+	void speak(String s)
+	{
+		System.out.println(s);
+	}
+}
+
+public class exp4_1
+{
+	public static void main (String args[])
+	{
+		xyr zbj;
+		zbj = new xyr();
+	}
+}
