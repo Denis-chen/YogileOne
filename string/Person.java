6524d59e9101f9f9b43712ee969574a3683e8c8b
20175223Yogile <575969143@qq.com>
2019-03-05 13:51:39 +0800

    wrong

---
+++ string/Person.java
@@ -0,0 +1,18 @@
+public class Person
+{
+	void speakHello()
+	{
+		System.out.print("");
+		System.out.print(" nice to meet you");
+	}
+}
+
+class Xiti 
+{
+	public static void main(String args[])
+	{
+		Person zhang = new Person();
+		zhang.speakHello();
+	}
+}
+
